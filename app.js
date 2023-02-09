fc14d8f4c792b64b13f1596feccc70a530be777f
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-02-09 21:03:24 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+
+        var cont = document.getElementById('cont');
+        var home = document.querySelector('.home');
+        var about = document.querySelector('.about');
+        var skills = document.querySelector('.skills');
+        var portofolio = document.querySelector('.portofolio');
+        var contact = document.querySelector('.contact');
+
+        cont.style.top="0px"
+
+        home.addEventListener('click', function(){
+            cont.style.top="0px"
+        });
+        about.addEventListener('click', function(){
+            cont.style.top="-450px"
+        });
+        skills.addEventListener('click', function(){
+            cont.style.top="-900px"
+        });
+        portofolio.addEventListener('click', function(){
+            cont.style.top="-1400px"
+        });
+        contact.addEventListener('click', function(){
+            cont.style.top="-1860px"
+        });
+
