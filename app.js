5afb2ad1fce4002bf7c66258ac46e61fbd18e8d2
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-02-28 17:22:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-        var cont3 = document.getElementById('cont');
+        var cont = document.getElementById('cont');
         var home = document.querySelector('.home');
         var about = document.querySelector('.about');
         var skills = document.querySelector('.skills');
